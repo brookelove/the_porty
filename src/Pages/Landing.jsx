88fe0ec5e9691759c9e7547810613d9f9e49f157
Brooke Love <savienloveb@gmail.com>
2023-06-25 16:17:41 -0400

    creating file path

---
+++ src/Pages/Landing.jsx
@@ -0,0 +1,10 @@
+import React from "react";
+import "../Assets/CSS/Pages/Landing.css";
+
+const Landing = () => {
+    return (
+        <h1>Landing</h1>
+    )
+}
+
+export default Landing;
---
+++ src/Pages/Work.jsx
@@ -0,0 +1,9 @@
+import React from "react";
+
+const Work = () => {
+    return (
+        <h1>Work</h1>
+    )
+}
+
+export default Work;
