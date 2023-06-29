731e2dd41d5ca224e97661cfdfd291f1778f7c39
Brooke Love <savienloveb@gmail.com>
2023-06-29 12:55:20 -0400

    getting 404 page to return back to home

--- src/Pages/PageNotFound.jsx
+++ src/Pages/PageNotFound.jsx
@@ -3,7 +3,7 @@ import React from "react";
 const PageNotFound = () => {
     return(
         <div>
-            <h1>Page Not Found</h1>
+            <h1>Sorry There is No Page with this Content! Please go back to <a href="/home">Home</a></h1>
         </div>
     )
 }
