2f5a0d41b1baea88c3b609791d289882079f5a26
brookelove <sblove@bu.edu>
2023-07-04 12:42:15 -0400

    creatinng homepage

--- src/Components/Contact.jsx
+++ src/Components/Contact.jsx
@@ -1,7 +1,7 @@
 import React from "react";
 const Contact = () => {
     return (
-        <div>
+        <div id="contactContainer">
             <h1>Contact</h1>
         </div>
     )
--- src/Pages/Home.jsx
+++ src/Pages/Home.jsx
@@ -1,8 +1,32 @@
 import React from "react";
-
+import "../Assets/CSS/Pages/Home.css"
+import seaVideo from "../Assets/Video/pexels-rostislav-uzunov-7513671 (2160p).mp4"
 const Home = () => {
     return (
-        <h1>Home</h1>
+        <div className="homeContainer">
+            <video playsInline autoPlay muted loop id="seaVideo">
+                <source src={seaVideo}></source>
+            </video>
+            <ul>
+                <li>
+                    <a>LI</a>
+                </li>
+                <li>
+                    <a>GH</a>
+                </li> 
+                <li>
+                    <a>IN</a>
+                </li>    
+                <li>
+                    <a>FB</a>
+                </li>    
+            </ul>
+            <section className="main">
+                <a href="/work"><h1>WORK</h1></a>
+                <a href="/about"><h1>ABOUT</h1></a>
+                <a href="#contactContainer"><h1>CONTACT</h1></a>
+            </section>
+        </div>
     )
 }
 
