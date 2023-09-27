b6440db1ea514d7c2bae3ff82ee62503a2199895
brookelove <sblove@bu.edu>
2023-09-27 15:26:02 -0400

    getting started on custom cursor

--- src/App.js
+++ src/App.js
@@ -4,16 +4,33 @@ import Home from "./Pages/Home";
 import Loading from "./Pages/Loading";
 import Work from "./Pages/Work";
 import PageNotFound from "./Pages/PageNotFound";
-import React, { useEffect, useState } from "react";
+import React, { useEffect, useState, useLayoutEffect } from "react";
 import NewHome from "./Components/NewHome";
 
 import { keepTheme } from "./utils/themes";
 function App() {
-  useEffect(() => {
-    keepTheme();
+  const [mousePosition, setMousePosition] = useState({
+    x: 0,
+    y: 0,
   });
+  console.log(mousePosition);
+
+  useEffect(() => {
+    const mouseMove = (e) => {
+      console.log(e);
+    };
+
+    window.addEventListener("mousemove", mouseMove);
+    keepTheme();
+    return () => {
+      window.removeEventListener("mousemove", mouseMove);
+    };
+    // });
+  }, []);
   return (
     <div className="App">
+      <div className="cursorDot" id="cursor-dot"></div>
+      <div className="cursorOutline" id="cursor-outline"></div>
       <BrowserRouter>
         <Routes>
           <Route path="/about" element={<About />} />
