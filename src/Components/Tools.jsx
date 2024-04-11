66a47f79750228e2225c9f6001945f514ecd31ff
brookelove <sblove@bu.edu>
2024-04-10 21:27:18 -0400

    updating the about section

--- src/Components/Tools.jsx
+++ src/Components/Tools.jsx
@@ -1,169 +1,64 @@
-import React, {useEffect, useRef} from "react";
+import React, {useEffect, useState} from "react";
 import resume from "../Assets/Images/SL_Resume_PDF.pdf"
-import { FaReact, FaSass } from "react-icons/fa6";
-import { FaAws, FaGithub, FaGitlab, FaNode, FaJava, FaPython, FaCss3Alt, FaHtml5, FaBootstrap, FaFigma } from "react-icons/fa";
-import { BiLogoJavascript, BiLogoJquery, BiLogoSlack } from "react-icons/bi";
-import { SiMysql, SiExpress, SiHandlebarsdotjs, SiJest, SiApollographql, SiKotlin, SiNotion, SiCanva, SiNginx, SiDocker} from "react-icons/si";
-import { VscJson } from "react-icons/vsc";
 import "../Assets/CSS/Components/Tools.css"
+import toolVideo from "../Assets/Images/tools.mp4"
 
 const Tools = () => {
+    const [shuffledSkills, setShuffledSkills] = useState([]); 
+    const skills = [
+        { name: "Java", category: "language" },
+        { name: "Python", category: "language" },
+        { name: "JavaScript", category: "language" },
+        { name: "HTML5", category: "language" },
+        { name: "CSS3", category: "language" },
+        { name: "Kotlin", category: "language" },
+        { name: "JSON", category: "language" },
+        { name: "React.js", category: "framework" },
+        { name: "Node.js", category: "framework" },
+        { name: "Express", category: "framework" },
+        { name: "Sassy CSS", category: "framework" },
+        { name: "Bootstrap", category: "framework" },
+        { name: "JQuery", category: "framework" },
+        { name: "Handlebars.js", category: "framework" },
+        { name: "Apollo GraphQL", category: "framework" },
+        { name: "Github", category: "version-control" },
+        { name: "Gitlab", category: "version-control" },
+        { name: "Nginx", category: "version-control" },
+        { name: "Docker", category: "version-control" },
+        { name: "Jest", category: "testing" },
+        { name: "AWS", category: "cloud" },
+        { name: "Figma", category: "design" },
+        { name: "Canva", category: "design" },
+        { name: "Notion", category: "productivity" }
+    ];
+    useEffect(() => {
+        const shuffledData = skills.sort(() => Math.random() - 0.5);
+        setShuffledSkills(shuffledData);
+    }, []);
 
     return(
         <div className="toolsContainer">
+            <div>
             <h1>SKILLS</h1>
-            <div className="d-even">
             <section>
-            <div className="portfolio-group d-c-center">
-            <h3>Languages:</h3>
-            <div className="scroller">
-                <ul className="item-list scroller-inner">
-                    <li className="item">
-                        <div className="tool-tip">Java</div>
-                        <FaJava className="reactIcon"/>
-                    </li>
-                    <li className="item">
-                        <div className="tool-tip">Python</div>
-                        <FaPython className="reactIcon"/>
-                    </li>
-                    <li className="item">
-                        <div className="tool-tip">JavaScript</div>
-                        <BiLogoJavascript className="reactIcon"/>
-                    </li>
-                    <li className="item">
-                        <div className="tool-tip">HTML5</div>
-                        <FaHtml5 className="reactIcon"/>
-                    </li>
-                    <li className="item">
-                        <div className="tool-tip">CSS3</div>
-                        <FaCss3Alt className="reactIcon"/>
-                    </li>
-                    <li className="item">
-                        <div className="tool-tip">Kotlin</div>
-                        <SiKotlin className="reactIcon"/>
-                    </li>
-                    <li className="item">
-                        <div className="tool-tip">JSON</div>
-                        <VscJson className="reactIcon"/>
-                    </li>
+                {/* languages */}
+                <video autoPlay muted loop id="shapes">
+                    <source  className="toolVideo"src={toolVideo} type="video/mp4"/>
+                </video>
+                <ul className="pillContainer">
+                {shuffledSkills.map((skill, index) => (
+                            <li key={index} className={skill.category}>
+                                {skill.name}
+                            </li>
+                        ))}
                 </ul>
-            </div>
-        </div>
-
-        <div className="portfolio-group d-c-center">
-            <h3>Frameworks and Libraries:</h3>
-            <div className="scroller">
-            <ul className="item-list scroller-inner">
-                <li className="item">
-                    <div className="tool-tip">React.js</div>
-                    <FaReact className="reactIcon"/>
-                </li>
-                <li className="item">
-                    <div className="tool-tip">Node.js</div>
-                    <FaNode className="reactIcon"/>
-                </li>
-                <li className="item">
-                    <div className="tool-tip">Express</div>
-                    <SiExpress className="reactIcon"/>
-                </li>
-                <li className="item">
-                    <div className="tool-tip">Sassy CSS</div>
-                    <FaSass className="reactIcon"/>
-                </li>
-                <li className="item">
-                 <div className="tool-tip">Bootstrap</div>
-                    <FaBootstrap className="reactIcon"/>
-                </li>
-                <li className="item">
-                <div className="tool-tip">JQuery</div>
-                    <BiLogoJquery className="reactIcon"/>
-                </li>
-                <li className="item">
-                    <div className="tool-tip">Handlebars.js</div>
-                    <SiHandlebarsdotjs className="reactIcon"/>
-                </li>
-                <li className="item">
-                    <div className="tool-tip">Apollo GraphQL</div>
-                    <SiApollographql className="reactIcon"/>
-                </li>
-                {/* Add other frameworks and libraries as needed */}
-            </ul>
-            </div>
-        </div>
-
-        <div className="portfolio-group d-c-center">
-            <h3>Version Control and Collaboration:</h3>
-            <div className="scroller">
-            <ul className="scroller-inner">
-                <li className="item">
-                    <div className="tool-tip">Github</div>
-                    <FaGithub className="reactIcon"/>
-                </li>
-                <li className="item">
-                    <div className="tool-tip">Gitlab</div>
-                    <FaGitlab className="reactIcon"/>
-                </li>
-                <li className="item">
-                    <div className="tool-tip">Nginx</div>
-                    <SiNginx className="reactIcon"/>
-                </li>
-                <li className="item">
-                    <div className="tool-tip">Docker</div>
-                    <SiDocker className="reactIcon"/>
-                </li>
-            </ul>
-            </div>
-        </div>
             </section>
-            <section>
-        <div className="portfolio-group d-c-center">
-            <h3>Testing and Quality Assurance:</h3>
-            <ul>
-                <li className="item">
-                    <div className="tool-tip">Jest</div>
-                    <SiJest className="reactIcon"/>
-                </li>
-            </ul>
-        </div>
-
-        <div className="portfolio-group d-c-center">
-            <h3>Cloud Services:</h3>
-            <ul>
-                <li className="item">
-                    <div className="tool-tip inProgressTool-tip">AWS in Progress</div>
-                    <FaAws className="reactIcon inProgress"/>
-                </li>
-            </ul>
-        </div>
-
-        <div className="portfolio-group d-c-center">
-            <h3>Design and Productivity:</h3>
-            <div className="scroller">
-            <ul className="scroller-inner">
-                <li className="item">
-                    <div className="tool-tip">Figma</div>
-                    <FaFigma className="reactIcon"/>
-                </li>
-                <li className="item">
-                    <div className="tool-tip">Canva</div>
-                    <SiCanva className="reactIcon"/>
-                </li>
-                <li className="item">
-                    <div className="tool-tip">Notion</div>
-                    <SiNotion className="reactIcon"/>
-                </li>
-            </ul>
-            </div>
-        </div>
-             </section>
-             </div>
-            <div className="resumeContainer">
             <a href={resume} target="_blank" rel="noreferrer">
             <button className="button">
                 RESUME
             </button>
             </a>
-        </div>
+            </div>
         </div>
     )
 }
